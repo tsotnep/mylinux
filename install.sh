c89bc8f0836e653b7f3f9dbd7a69d7a72d396611
Tsotne Putkaradze <tsotnep@gmail.com>
2016-10-27 02:33:26 +0300

    either: moved a file, changed variable name, formatted the code or something simillar, no need for special message

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #tools to install
 
-sudo apt install -y pydf inxi ack-grep pydf picocom python-pygments xclip rsync vim tmux
+sudo apt install -y pydf inxi ack-grep pydf picocom python-pygments xclip rsync vim tmux dropbox
