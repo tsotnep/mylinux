681321b1bf7f52b4d5762f38ed2c99f885511505
Tsotne Putkaradze <tsotnep@gmail.com>
2016-10-28 12:31:24 +0300

    either: moved a file, changed variable name, formatted the code or something simillar, no need for special message

--- install.sh
+++ install.sh
@@ -5,10 +5,19 @@ cd ~/ws
 mkdir workspace git logs vpn web Dropbox ssh
 cd git
 git clone https://github.com/tsotnep/mylinux
-cd mylinux 
+cd mylinux
+
+# backup files
+cp ~/.bashrc ~/.bashrc_bak 2>/dev/null
+cp ~/.vimrc ~/.vimrc_bak 2>/dev/null
+cp ~/.tmux.conf ~/.tmux.conf_bak 2>/dev/null
+cp /etc/.bash.bashrc /etc/.bash.bashrc_bak 2>/dev/null
+
+# write new files
 ln -f .bashrc ~/
 ln -f .vimrc ~/
 ln -f .tmux.conf ~/
+ln -f .bash.bashrc /etc/
 
 # setup tmux, install package manager, ctrl+A + I -to install all packages
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
