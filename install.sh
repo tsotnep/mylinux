be400cc249831919fc657e078653bade5f67ee63
tsotnep <tsotnep@gmail.com>
2016-11-11 19:01:23 +0200

    msg:

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 source ~/.bashrc
 touch ../.check
+source ../.check
 rm ../.check
 echo "Start.."
+echo "$VWS"
+echo "$VBK"
+echo "$VTMUX"
+echo "$VGIT"
+echo "$VID"
 
-
-if [ "$WS" != "1" ]
+if [ "$VWS" != "1" ]
 then
-# setup directories 
+  # setup directories 
   mkdir ~/ws
   cd ~/ws
   mkdir workspace git logs vpn web Dropbox
@@ -14,11 +19,11 @@ then
   git clone https://github.com/tsotnep/mylinux
   cd mylinux
 fi
-echo "WS=1" >> ../.check
+echo "VWS=1" >> ../.check
 
-if [ "$BK" != "1" ]
+if [ "$VBK" != "1" ]
 then
-# backup files
+  # backup files
   cp ~/.bashrc ~/.bashrc_bak 2>/dev/null
   cp ~/.bash_profile ~/.bash_profile_bak 2>/dev/null
   echo "source ~/.bashrc" > ~/.bash_profile 
@@ -26,24 +31,20 @@ then
   cp ~/.tmux.conf ~/.tmux.conf_bak 2>/dev/null
   #cp /etc/.bash.bashrc /etc/.bash.bashrc_bak 2>/dev/null
   
-# write new files
+  # write new files
   ln -fs `pwd`/.bashrc $HOME
   ln -fs `pwd`/.vimrc $HOME
   ln -fs `pwd`/.tmux.conf $HOME
 fi
-echo "BK=1" >> ../.check
+echo "VBK=1" >> ../.check
 
 
-if [ "$TMUX" != "1" ]
+if [ "$VTMUX" != "1" ]
 then
-# setup tmux, install package manager, ctrl+A + I -to install all packages
+  # setup tmux, install package manager, ctrl+A + I -to install all packages
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# install tools
-  sudo apt install -y pydf inxi ack-grep pydf picocom python-pygments xclip rsync vim dropbox git vim glipper tmux libevent-dev ncurses-dev most pinta google-chrome-stable
-  sudo apt autoremove -y
-
-# install tmux2.2
+  # install tmux2.2
   if [ ! -d ~/ws/git/tmux ]
   then
     mkdir ~/ws/git/tmux
@@ -54,53 +55,50 @@ then
     ./configure && make
     sudo ln -sf `pwd`/tmux /usr/local/bin/
   fi
+
+  if [ "$VID" != "1" ]
+  then
+    #download and install id software
+    wget https://installer.id.ee/media/install-scripts/install-open-eid.sh
+    bash install-open-eid.sh
+    rm install-open-eid.sh*
+  fi
+  echo "VID=1" >> ../.check 
+  # install tools
+  sudo apt install -y pydf inxi ack-grep pydf picocom python-pygments xclip rsync vim dropbox git vim glipper tmux libevent-dev ncurses-dev most pinta google-chrome-stable
+  sudo apt autoremove -y
+
+
 fi
-echo "TMUX=1" >> ../.check
+echo "VTMUX=1" >> ../.check
 
 
-if [ "$GIT" != "1" ]
+if [ "$VGIT" != "1" ]
 then
-#config ssh to reuse tunnels
+  #config ssh to reuse tunnels
   echo host \* >> ~/.ssh/config
   echo "ControlMaster auto" >> ~/.ssh/config
   echo "ControlPath ~/.ssh/ssh_mux_%h_%p_%r" >> ~/.ssh/config
 
-#config git
+  #config git
   git config --global user.email "tsotnep@gmail.com"
   git config --global user.name "tsotnep"
   git push --set-upstream origin master
   git config --global push.default matching
 
-#config mylinux to use ssh instead of https
+  #config mylinux to use ssh instead of https
   cd ~/ws/git/mylinux
   git remote remove origin
   git remote add origin git@github.com:tsotnep/mylinux.git
 
   if [ ! -f ~/.ssh/id_rsa ]
   then
-  #generate SSH key if necessary
+    #generate SSH key if necessary
     ssh-keygen -t rsa -C "tsotnep@gmail.com"
     ssh-add ~/.ssh/id_rsa
   fi
 fi
-echo "GIT=1" >> ../.check
-
-
-
-
-
-
-
-
-
-if [ "$ID" != "1" ]
-then
-#download and install id software
-  wget https://installer.id.ee/media/install-scripts/install-open-eid.sh
-  bash install-open-eid.sh
-  rm install-open-eid.sh
-fi
-echo "ID=1" >> ../.check 
+echo "VGIT=1" >> ../.check
 
 
 echo "Done.."
